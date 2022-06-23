bbcea5e2b35f60f8804f4f22a803711ca1c96c5a
Brian Blankenship <c0dezer019@zohomail.com>
2022-06-23 17:34:37 -0500

    Changing pre-commit.

--- src/ui/components/subcomponents/temperature.tsx
+++ src/ui/components/subcomponents/temperature.tsx
@@ -1,15 +1,10 @@
-import { FC, ReactElement } from 'react';
-import { Container } from 'react-bootstrap';
+import { FC, ReactElement } from "react";
+import { Container } from "react-bootstrap";
 
-import { selectTemp } from '../../../redux/reducers/weatherSlice';
+import { selectTemp } from "../../../redux/reducers/weatherSlice";
 
 const Temperature: FC = (): ReactElement => {
-
-return (
-    <Container>
-      {selectTemp}
-    </Container>
-  );
+  return <Container>{selectTemp}</Container>;
 };
 
 export default Temperature;
